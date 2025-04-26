40e910658ab569cc84a0fcfb28781a7408a30345
Dinhduy01 <dinhduy2012001@gmail.com>
2025-04-26 19:30:53 +0700

    update

--- BackEnd/src/middleware/cors.js
+++ BackEnd/src/middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
